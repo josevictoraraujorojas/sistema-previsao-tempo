53c51e8a3b9fe19186e7450dbfea8c406bb9b101
josevictoraraujorojas <kaneki1212112121@gmail.com>
2026-05-03 10:40:44 -0300

    Initial commit

---
+++ src/main/java/org/example/sistemaprevisaotempo/SistemaPrevisaoTempoApplication.java
@@ -0,0 +1,13 @@
+package org.example.sistemaprevisaotempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPrevisaoTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPrevisaoTempoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sistemaprevisaotempo/SistemaPrevisaoTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sistemaprevisaotempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPrevisaoTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
